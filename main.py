129a780b0740e5168095b98f39ce576360a16a0d
Super-DLT <learndlt5@gmail.com>
2021-05-09 17:38:02 +0430

    Done

---
+++ main.py
@@ -0,0 +1,17 @@
+#1. Create a greating for the Program
+
+print("Welcome to Band Generator :)\n")
+
+#2. Ask the user for the city that they grew up in
+
+user_city = input("What is the city that you grew in?\n")
+
+#3. Ask the user for the name of a pet
+
+user_pet = input("what is your pet's name?\n")
+
+#4. Combine the name of their city and pet  and show them their band name
+
+print("Your Band name could be "+user_city+" "+user_pet)
+
+#5. Make sure the input cursor shows on a new line
